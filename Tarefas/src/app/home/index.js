a3d76512b058f48520f0a957867d7325dfa786dd
komachi1ne <isabellevasconcelos862@gmail.com>
2024-06-13 12:25:32 -0300

    fazer npm i

--- Tarefas/src/app/home/index.js
+++ Tarefas/src/app/home/index.js
@@ -1,79 +1,135 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { Link } from "expo-router";
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, SafeAreaView, ImageBackground, TextInput, TouchableOpacity, FlatList, Alert } from 'react-native';
+import { StatusBar  } from 'expo-status-bar';
+import { Ionicons } from '@expo/vector-icons';
+import { styles } from './style';
+import ItemList from '../../components/ItemList';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function Welcome() {
+export default function Home() {
+  const [textInput, setTextInput] = useState('');
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    getItemsFromDevice();
+  }, []);
+  useEffect(() => {
+    saveItemToDevice(items);
+  }, [items]);
+
+  const saveItemToDevice = async (items) => {
+    try {
+      const itemJson = JSON.stringify(items);
+      await AsyncStorage.setItem('galloshoppinglist', itemJson);
+    } catch (error) {
+      console.log(`Erro: ${error}`);
+    }
+  }
+
+  const getItemsFromDevice = async() => {
+    try {
+      const items = await AsyncStorage.getItem('galloshoppinglist');
+      if (items != null){
+        setItems(JSON.parse(items));
+      }
+    } catch (error) {
+      console.log(`Erro: ${error}`);
+    }
+  }
+
+  const addItem = () => {
+    // console.log(textInput);
+    if (textInput == ''){
+      Alert.alert('Ocorreu um problema :(', 'Por favor, informe o nome do produto!!');
+    } else {
+      const newItem = {
+        id: Math.random(),
+        name: textInput,
+        bought: false
+      };
+      setItems([...items, newItem]);
+      setTextInput('');
+    }
+  }
+
+  const markItemBought = itemId => {
+    const newItems = items.map((item) => {
+      if (item.id == itemId){
+        return { ...item, bought: true }
+      }
+      return item;
+    });
+    setItems(newItems);
+  }
+
+  const unmarkItemBought = itemId => {
+    const newItems = items.map((item) => {
+      if (item.id == itemId){
+        return { ...item, bought: false }
+      }
+      return item;
+    });
+    setItems(newItems);
+  }
+
+  const removeItem = itemId => {
+    Alert.alert('Excluir produto?', 'Confirma a exclus o deste produto?', 
+    [
+      {
+        text: 'Sim', onPress: () => {
+          const newItems = items.filter(item => item.id != itemId);
+          setItems(newItems);
+        }
+      },
+      {
+        text: 'Cancelar',
+        style: 'cancel'
+      }
+    ]);
+  }
+
+  const removeAll = () => {
+    Alert.alert('Limpar lista?', 'Confirme a exclus o de todos os produtos de sua lista?', 
+    [{
+      text: 'sim', onPress: () => {setItems([])}
+    },{
+      text: 'cancelar', style: 'cancel', 
+    }]);
+  }
+
   return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <Image
-          source={require("../assets/1.jpg")}
-          style={styles.image}
+    <SafeAreaView style={{ flex: 1 }}>
+      <ImageBackground source={require('../../assets/background.jpg')} resizeMode='repeat' style={{flex: 1, justifyContent: 'flex-start'}}>
+        
+        <View style={styles.header}>
+          <Text style={styles.title}>Lista de produtos</Text>
+          <Ionicons name="trash" size={32} color='#fff' onPress={removeAll} />
+        </View>
+
+        {/* Lista de produtos */}
+        <FlatList
+          contentContainerStyle={{ padding: 20, paddingBottom:100, color:"#fff" }}
+          data={items}
+          renderItem={({item}) => <ItemList item={item} markItem={markItemBought} unmarkItem={unmarkItemBought} removeItem={removeItem}/>}
         />
-      </View>
-      <View style={styles.content}>
-        <Text style={styles.title}>Gallo Shopping List</Text>
-        <Text style={styles.text}>
-          Monte sua lista de compras e não esqueça mais o que precisa
-          comprar ao sair de casa!!!
-          </Text>
-        <Link style={styles.button} href={"/home"}>
-          <Text style={styles.buttonText}>Acessar</Text>
-        </Link>
-      </View>
+
+        <View style={styles.footer}>
+          <View style={styles.inputContainer}>
+            <TextInput 
+              color = '#fff'
+              fontSize={18}
+              placeholderTextColor="#fff"
+              placeholder='Digite o Nome do produto...'
+              value={textInput}
+              onChangeText={(text) => setTextInput(text)}
+            />
+          </View>
+          <TouchableOpacity style={styles.iconContainer} onPress={addItem}>
+            <Ionicons name='add' size={36} color='#fff'/>
+          </TouchableOpacity>
+        </View>
+
+      </ImageBackground>
       <StatusBar style="light" backgroundColor='#000' />
-    </View>
-  );
+    </SafeAreaView>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000000',
-  },
-  imageContainer: {
-    flex: 2,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  image : {
-    width: 230,
-    height: 230,
-    borderRadius: 115,
-    resizeMode: 'cover',
-  },
-  content: {
-    flex: 1,
-    backgroundColor: "#dadada",
-    borderTopLeftRadius: 25,
-    borderTopRightRadius: 25,
-    paddingHorizontal: '5%',
-  },
-  title: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    marginTop: 28,
-    marginBottom: 20,
-    textAlign: 'center',
-  },
-  text: {
-    color: 'gray',
-    fontSize: 16,
-  },
-  button: {
-    position: 'absolute',
-    backgroundColor: '#000',
-    borderRadius: 50,
-    bottom: '15%',
-    paddingVertical: 15,
-    width: '60%',
-    alignSelf: 'center',
-    textAlign: 'center',
-  },
-  buttonText: {
-  color: '#fff',
-  fontSize: 22,
-  fontWeight: 'bold'
-  },
-
-});
--- Tarefas/src/app/index.js
+++ Tarefas/src/app/index.js
@@ -1,12 +1,27 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Link } from "expo-router";
 
-export default function Page() {
+export default function Welcome() {
   return (
     <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("../assets/1.jpg")}
+          style={styles.image}
+        />
       </View>
+      <View style={styles.content}>
+        <Text style={styles.title}>Gallo Shopping List</Text>
+        <Text style={styles.text}>
+          Monte sua lista de compras e não esqueça mais o que precisa
+          comprar ao sair de casa!!!
+          </Text>
+        <Link style={styles.button} href={"/home"}>
+          <Text style={styles.buttonText}>Acessar</Text>
+        </Link>
+      </View>
+      <StatusBar style="light" backgroundColor='#000' />
     </View>
   );
 }
@@ -14,21 +29,51 @@ export default function Page() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    padding: 24,
+    backgroundColor: '#000000',
   },
-  main: {
+  imageContainer: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image : {
+    width: 230,
+    height: 230,
+    borderRadius: 115,
+    resizeMode: 'cover',
+  },
+  content: {
     flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
+    backgroundColor: "#dadada",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingHorizontal: '5%',
   },
   title: {
-    fontSize: 64,
-    fontWeight: "bold",
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: 28,
+    marginBottom: 20,
+    textAlign: 'center',
   },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
+  text: {
+    color: 'gray',
+    fontSize: 16,
   },
+  button: {
+    position: 'absolute',
+    backgroundColor: '#000',
+    borderRadius: 50,
+    bottom: '15%',
+    paddingVertical: 15,
+    width: '60%',
+    alignSelf: 'center',
+    textAlign: 'center',
+  },
+  buttonText: {
+  color: '#fff',
+  fontSize: 22,
+  fontWeight: 'bold'
+  },
+
 });
